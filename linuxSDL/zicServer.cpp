0755e90a64696bdd8148262870abcddc42910750
Alexandre Piel <Alexandre.Piel@zooplus.com>
2022-05-29 19:22:00 +0200

    save

--- linuxSDL/zicServer.cpp
+++ linuxSDL/zicServer.cpp
@@ -170,7 +170,7 @@ int main(int argc, char* args[])
     screenSurface = SDL_GetWindowSurface(window);
     //SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
 
-    draw_string(screenSurface, "Hello Alex # ! Yo", 10, 10, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF), 2);
+    draw_string(screenSurface, "Hello Alex # !\nYo", 10, 0, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF), 2);
 
     SDL_UpdateWindowSurface(window);
 
