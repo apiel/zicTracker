3572fbe7d351065819563a868e996be14486312d
Alexandre Piel <Alexandre.Piel@zooplus.com>
2022-07-04 18:50:13 +0200

    fix

--- app/app_patterns.h
+++ app/app_patterns.h
@@ -55,7 +55,8 @@ public:
                     // printf("exit loop %d at step %d\n", pos + 1, count);
                     break;
                 }
-                step->instrument = stepData[0] == SAME_INSTRUMENT_SYMBOL ? prevInstrument : 'A' - stepData[0];
+                step->instrument = stepData[0] == SAME_INSTRUMENT_SYMBOL ? prevInstrument : (stepData[0] - 'A');
+                printf("Instrument %c val %d\n", stepData[0], step->instrument);
                 prevInstrument = step->instrument;
                 step->note = stepData[2] == '-' ? 0 : Zic::charNotetoInt(stepData[2], stepData[3], stepData[4]);
                 step->slide = stepData[6] == '1';
