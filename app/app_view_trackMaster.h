f8d7029d5465eb115ffd88bc88d42e802a5ef595
Alexandre Piel <Alexandre.Piel@zooplus.com>
2022-07-12 18:53:21 +0200

    master track tbd

--- app/app_view_trackMaster.h
+++ app/app_view_trackMaster.h
@@ -5,41 +5,42 @@
 #include "./app_tracks.h"
 #include "./app_view_table.h"
 
-// TODO rename to track master
-// where multiple tracks can be sent to it
-// and where we can apply action for several tracks at once
-//
-// the keyboard should not be note but detune semitone value
-// of 3 rows.
-class App_View_TrackMaster : public App_View_Table<7, 6, 7> {
-    // class App_View_TrackLoop : public App_View_Table<5, 4, 7> {
+class App_View_TrackMaster : public App_View_Table<7, 6, 6> {
 protected:
     App_Tracks* tracks;
 
 public:
     App_View_TrackMaster(App_Tracks* _tracks)
-        : App_View_Table(9)
+        : App_View_Table(7)
         , tracks(_tracks)
     {
     }
 
+void initDisplay(App_Display* display) override
+    {
+        strcpy(display->text, "Master track\n\nto be implemented");
+    }
+
     void renderCell(App_Display* display, uint16_t pos, uint16_t row, uint8_t col)
     {
-        uint8_t note = Zic::naturalNotes[pos];
-        if (cursor == pos) {
-            display->setCursor(2, 1);
-        }
-        sprintf(display->text + strlen(display->text), "%c%s%d",
-            tracks->looper->nextState.detune == note ? '>' : ' ', Zic::getNoteStr(note), Zic::getNoteOctave(note));
+        // TODO there is no point to have this GRID.
+        // Let just keep it simple with the same UI as for track
+        // int8_t detune = pos - 24;
+        // if (cursor == pos) {
+        //     display->setCursor(3);
+        // }
+        // sprintf(display->text + strlen(display->text), "%c%02d ", detune < 0 ? '-' : '+', abs(detune));
+        // sprintf(display->text + strlen(display->text), "%c%s%d",
+        //     tracks->looper->nextState.detune == note ? '>' : ' ', Zic::getNoteStr(note), Zic::getNoteOctave(note));
     }
 
     uint8_t update(UiKeys* keys, App_Display* display)
     {
         if (keys->A) {
-            uint8_t note = Zic::naturalNotes[cursor];
-            tracks->looper->nextState.set(note);
-            render(display);
-            return VIEW_CHANGED;
+            // uint8_t note = Zic::naturalNotes[cursor];
+            // tracks->looper->nextState.set(note);
+            // render(display);
+            // return VIEW_CHANGED;
         }
         return App_View_Table::update(keys, display);
     }
