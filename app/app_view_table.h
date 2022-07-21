3b3b4dcad6449c069992260e53938c3653f0fe67
Alexandre Piel <Alexandre.Piel@zooplus.com>
2022-07-21 14:08:39 +0200

    sel

--- app/app_view_table.h
+++ app/app_view_table.h
@@ -13,6 +13,14 @@ public:
         return VIEW_NONE;
     }
 
+    virtual void updateEnd()
+    {
+    }
+
+    virtual void updateStart()
+    {
+    }
+
     virtual bool isSelectable(uint8_t row, uint8_t col)
     {
         return false;
@@ -24,6 +32,7 @@ protected:
     const uint8_t ROW_COUNT;
     const uint8_t COL_COUNT;
     App_View_TableField** fields;
+    bool updating = false;
 
     App_View_TableField* getSelectedField()
     {
@@ -112,113 +121,28 @@ public:
     uint8_t update(UiKeys* keys, App_Display* display)
     {
         if (keys->A) {
-            return getSelectedField()->update(keys, display);
-        } else {
-            if (keys->Up) {
-                selectNextRow(-1);
-            } else if (keys->Down) {
-                selectNextRow(+1);
-            } else if (keys->Left) {
-                selectNextCol(-1);
-            } else if (keys->Right) {
-                selectNextCol(+1);
+            if (!updating) {
+                getSelectedField()->updateStart();
+                updating = true;
             }
-            render(display);
-            return VIEW_CHANGED;
+            return getSelectedField()->update(keys, display);
         }
-        return VIEW_NONE;
+        if (updating) {
+            getSelectedField()->updateEnd();
+            updating = false;
+        }
+        if (keys->Up) {
+            selectNextRow(-1);
+        } else if (keys->Down) {
+            selectNextRow(+1);
+        } else if (keys->Left) {
+            selectNextCol(-1);
+        } else if (keys->Right) {
+            selectNextCol(+1);
+        }
+        render(display);
+        return VIEW_CHANGED;
     }
 };
-
-// template <uint8_t VISIBLE_ROW, uint8_t VISIBLE_COL, uint8_t TOTAL_COL>
-// class App_View_Table : App_View {
-// public:
-//     uint16_t startPos = 0;
-//     uint16_t cursor = 0;
-//     uint16_t TOTAL_ROW = 0;
-//     uint16_t TOTAL_CELL = 0;
-
-//     App_View_Table(uint16_t totalRow)
-//         : TOTAL_ROW(totalRow)
-//         , TOTAL_CELL(totalRow * TOTAL_COL)
-//     {
-//     }
-
-//     virtual void renderCell(App_Display* display, uint16_t pos, uint16_t row, uint8_t col) = 0;
-
-//     virtual void initDisplay(App_Display* display)
-//     {
-//         strcpy(display->text, "");
-//     }
-
-//     virtual void colSeparator(App_Display* display)
-//     {
-//         strcat(display->text, " ");
-//     }
-
-//     virtual void endRow(App_Display* display, uint16_t row)
-//     {
-//         strcat(display->text, "\n");
-//     }
-
-//     virtual void startRow(App_Display* display, uint16_t row)
-//     {
-//     }
-
-//     void render(App_Display* display)
-//     {
-//         initDisplay(display);
-//         for (uint16_t row = 0, n = 0; row < VISIBLE_ROW; row++, n += TOTAL_COL - VISIBLE_COL) {
-//             startRow(display, row);
-//             for (uint8_t col = 0; col < VISIBLE_COL; col++, n++) {
-//                 if (col != 0) {
-//                     colSeparator(display);
-//                 }
-//                 renderCell(display, n + startPos, row, col);
-//             }
-//             endRow(display, row);
-//         }
-//     }
-
-//     uint8_t update(UiKeys* keys, App_Display* display)
-//     {
-//         if (keys->Up || keys->Down || keys->Right || keys->Left) {
-//             if (keys->Up) {
-//                 if ((int16_t)cursor >= TOTAL_COL) {
-//                     cursor -= TOTAL_COL;
-//                     if (cursor < startPos) {
-//                         startPos -= TOTAL_COL;
-//                     }
-//                 }
-//             } else if (keys->Down) {
-//                 if (cursor + TOTAL_COL < TOTAL_CELL) {
-//                     cursor += TOTAL_COL;
-//                     if (cursor >= startPos + (TOTAL_COL * VISIBLE_ROW)) {
-//                         startPos += TOTAL_COL;
-//                     }
-//                 }
-//             } else if (keys->Right) {
-//                 if (cursor % TOTAL_COL < TOTAL_COL - 1) {
-//                     cursor++;
-//                     if ((cursor % TOTAL_COL) - (startPos % TOTAL_COL) == VISIBLE_COL) {
-//                         startPos++;
-//                     }
-//                 }
-//             } else if (keys->Left) {
-//                 if (cursor % TOTAL_COL > 0) {
-//                     cursor--;
-//                     if (cursor % TOTAL_COL < startPos % TOTAL_COL) {
-//                         startPos--;
-//                     }
-//                 }
-//             }
-//             // SDL_Log("startPos %d,%d cursor %d,%d", startPos, startPos % TOTAL_COL, cursor, cursor % TOTAL_COL);
-//             // SDL_Log("%d == %d", (cursor % TOTAL_COL) - (startPos % TOTAL_COL), VISIBLE_COL);
-//             render(display);
-//             return VIEW_CHANGED;
-//         }
-//         return VIEW_NONE;
-//     }
-// };
 
 #endif
--- app/app_view_track.h
+++ app/app_view_track.h
@@ -13,6 +13,8 @@
 class App_View_TrackRow : public App_View_TableField {
 protected:
     App_Tracks* tracks;
+    Zic_Seq_Loop_State newState;
+    bool updating = false;
 
 public:
     App_View_TrackRow(App_Tracks* _tracks)
@@ -38,11 +40,25 @@ public:
             }
             uint8_t trackId = col - 1;
             Zic_Seq_Loop_State* state = &tracks->tracks[trackId]->looper.nextState;
-            //     if (updatingState && cursor == pos) {
-            //         state = &newState;
-            //     }
+            if (updating && col == selectedCol && row == selectedRow) {
+                state = &newState;
+            }
             renderValue(display, trackId, state);
         }
+    }
+
+    void updateStart()
+    {
+        Zic_Seq_Loop_State* nextState = &tracks->looper->nextState;
+        newState.set(nextState);
+        updating = true;
+    }
+
+    void updateEnd()
+    {
+        Zic_Seq_Loop_State* nextState = &tracks->looper->nextState;
+        nextState->set(&newState);
+        updating = false;
     }
 };
 
@@ -84,6 +100,12 @@ public:
             strcat(display->text, " OFF");
         }
     }
+
+    uint8_t update(UiKeys* keys, App_Display* display)
+    {
+        newState.togglePlay();
+        return VIEW_CHANGED;
+    }
 };
 
 class App_View_TrackPattern : public App_View_TrackRow {
@@ -102,6 +124,23 @@ public:
     {
         sprintf(display->text + strlen(display->text), " %03d", state->pattern->id + 1);
     }
+
+    uint8_t update(UiKeys* keys, App_Display* display)
+    {
+        int8_t direction = 0;
+        if (keys->Right) {
+            direction = 1;
+        } else if (keys->Left) {
+            direction = -1;
+        } else if (keys->Up) {
+            direction = 10;
+        } else if (keys->Down) {
+            direction = -10;
+        }
+        uint16_t id = newState.pattern->id ? newState.pattern->id : PATTERN_COUNT;
+        newState.pattern = &tracks->patterns->patterns[(id + direction) % PATTERN_COUNT];
+        return VIEW_CHANGED;
+    }
 };
 
 class App_View_TrackDetune : public App_View_TrackRow {
@@ -119,6 +158,20 @@ public:
     void renderValue(App_Display* display, uint8_t trackId, Zic_Seq_Loop_State* state)
     {
         sprintf(display->text + strlen(display->text), " %c%02d", state->detune < 0 ? '-' : '+', abs(state->detune));
+    }
+
+    uint8_t update(UiKeys* keys, App_Display* display)
+    {
+        if (keys->Right) {
+            newState.set(newState.detune + 1);
+        } else if (keys->Up) {
+            newState.set(newState.detune + 12);
+        } else if (keys->Left) {
+            newState.set(newState.detune - 1);
+        } else if (keys->Down) {
+            newState.set(newState.detune - 12);
+        }
+        return VIEW_CHANGED;
     }
 };
 
@@ -143,9 +196,7 @@ public:
 
 class App_View_Track : public App_View_Table {
 protected:
-    App_Tracks* tracks;
-    Zic_Seq_Loop_State newState;
-    bool updatingState = false;
+App_Tracks* tracks;
 
     App_View_TrackHeader fieldHeader;
     App_View_TrackSequence sequenceField;
@@ -206,59 +257,11 @@ public:
 
     uint8_t update(UiKeys* keys, App_Display* display)
     {
+        int8_t trackId = selectedCol - 1;
+                if (trackId != tracks->trackId) {
+            tracks->select(trackId);
+        }
         return App_View_Table::update(keys, display);
-        // int8_t row = cursor / VIEW_TRACK_COL;
-        // int8_t col = cursor % VIEW_TRACK_COL;
-        // int8_t trackId = col;
-        // Zic_Seq_Loop_State* nextState = &tracks->looper->nextState;
-        // if (keys->A) {
-        //     if (!updatingState) {
-        //         newState.set(nextState);
-        //         updatingState = true;
-        //     }
-        //     switch (row) {
-        //     case 0:
-        //         newState.togglePlay();
-        //         break;
-        //     case 1: {
-        //         int8_t direction = 0;
-        //         if (keys->Right) {
-        //             direction = 1;
-        //         } else if (keys->Left) {
-        //             direction = -1;
-        //         } else if (keys->Up) {
-        //             direction = 10;
-        //         } else if (keys->Down) {
-        //             direction = -10;
-        //         }
-        //         uint16_t id = newState.pattern->id ? newState.pattern->id : PATTERN_COUNT;
-        //         newState.pattern = &tracks->patterns->patterns[(id + direction) % PATTERN_COUNT];
-        //         break;
-        //     }
-        //     case 2:
-        //         if (keys->Right) {
-        //             newState.set(newState.detune + 1);
-        //         } else if (keys->Up) {
-        //             newState.set(newState.detune + 12);
-        //         } else if (keys->Left) {
-        //             newState.set(newState.detune - 1);
-        //         } else if (keys->Down) {
-        //             newState.set(newState.detune - 12);
-        //         }
-        //         break;
-        //     }
-        //     App_View_Table::render(display);
-        //     return VIEW_CHANGED;
-        // } else if (updatingState) {
-        //     // apply newState to nextState only once A is release
-        //     nextState->set(&newState);
-        //     updatingState = false;
-        // }
-        // uint8_t res = App_View_Table::update(keys, display);
-        // if (trackId != tracks->trackId) {
-        //     tracks->select(trackId);
-        // }
-        // return res;
     }
 };
 
