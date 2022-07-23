798c3e829f3859d010e821a98283950935de05d3
Alexandre Piel <Alexandre.Piel@zooplus.com>
2022-07-23 15:10:05 +0200

    pat steps

--- app/app_view_pattern.h
+++ app/app_view_pattern.h
@@ -4,7 +4,8 @@
 #include "./app_display.h"
 #include "./app_view.h"
 
-#define VIEW_PATTERN_ROW 4 // 4 + MAX_STEPS_IN_PATTERN
+#define VIEW_PATTERN_ROW_HEADERS 4
+#define VIEW_PATTERN_ROW (VIEW_PATTERN_ROW_HEADERS + MAX_STEPS_IN_PATTERN)
 #define VIEW_PATTERN_COL 5
 
 class App_View_PatternHeader : public App_View_TableField {
@@ -82,29 +83,171 @@ public:
     }
 };
 
+class App_View_PatternStep : public App_View_TableField {
+protected:
+    uint8_t* currentPatternId;
+    App_Patterns* patterns;
+
+    Zic_Seq_Pattern* getPattern()
+    {
+        return &patterns->patterns[*currentPatternId];
+    }
+
+public:
+    App_View_PatternStep(App_Patterns* _patterns, uint8_t* _currentPatternId)
+        : patterns(_patterns)
+        , currentPatternId(_currentPatternId)
+    {
+    }
+
+    bool isSelectable(uint8_t row, uint8_t col) override
+    {
+        return col != 0;
+    }
+
+    void render(App_Display* display, uint8_t row, uint8_t col, uint8_t selectedRow, uint8_t selectedCol)
+    {
+        if (selectedRow == row && selectedCol == col) {
+            display->setCursor(3);
+        }
+        uint8_t stepPos = row - VIEW_PATTERN_ROW_HEADERS;
+        Zic_Seq_Step* step = &getPattern()->steps[stepPos];
+        switch (col) {
+        case 0:
+            sprintf(display->text + strlen(display->text), "%3d ", stepPos + 1);
+            break;
+
+        case 1:
+            sprintf(display->text + strlen(display->text), "  %c ", step->instrument + 'A');
+            break;
+
+        case 2:
+            sprintf(display->text + strlen(display->text), "%3d ", step->note);
+            break;
+
+        case 3:
+            sprintf(display->text + strlen(display->text), "%3d ", step->velocity);
+            break;
+
+        case 4:
+            sprintf(display->text + strlen(display->text), "%s", step->slide ? "ON " : "OFF");
+            break;
+        }
+    }
+
+    uint8_t update(UiKeys* keys, App_Display* display, uint8_t row, uint8_t col)
+    {
+        // int8_t direction = 0;
+        // if (keys->Right) {
+        //     direction = 1;
+        // } else if (keys->Left) {
+        //     direction = -1;
+        // } else if (keys->Up) {
+        //     direction = 10;
+        // } else if (keys->Down) {
+        //     direction = -10;
+        // }
+        // *currentPatternId = (*currentPatternId + PATTERN_COUNT + direction) % PATTERN_COUNT;
+        return VIEW_CHANGED;
+    }
+};
 
 class App_View_Pattern : public App_View_Table {
 protected:
-    App_Patterns* patterns;
+    // App_Patterns* patterns;
     uint8_t currentPatternId = 0;
 
     App_View_PatternHeader headerField;
     App_View_PatternStepHeader stepHeaderField;
+    App_View_PatternStep stepField;
 
     App_View_TableField* fields[VIEW_PATTERN_ROW * VIEW_PATTERN_COL] = {
         // clang-format off
         &headerField, &headerField, NULL, NULL, NULL,
         &headerField, &headerField, NULL, NULL, NULL,
         NULL, NULL, NULL, NULL, NULL,
-        &stepHeaderField, &stepHeaderField, &stepHeaderField, &stepHeaderField, &stepHeaderField, 
+        &stepHeaderField, &stepHeaderField, &stepHeaderField, &stepHeaderField, &stepHeaderField,
+        // 1
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        // 11
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        //21
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        // 31
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        // 41
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        // 51
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        // 61
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        &stepField, &stepField, &stepField, &stepField, &stepField,
+        // &stepField, &stepField, &stepField, &stepField, &stepField,
         // clang-format on
     };
 
 public:
     App_View_Pattern(App_Patterns* _patterns)
         : App_View_Table(fields, VIEW_PATTERN_ROW, VIEW_PATTERN_COL)
-        , patterns(_patterns)
+        // , patterns(_patterns)
         , headerField(&currentPatternId)
+        , stepField(_patterns, &currentPatternId)
     {
         initSelection();
     }
