fedda79e8c20b91d82a77bb85433e95c33025b0f
Alexandre Piel <Alexandre.Piel@zooplus.com>
2022-12-02 20:40:28 +0100

    js

--- linuxSDL/instruments/synth01/main.js
+++ linuxSDL/instruments/synth01/main.js
@@ -1,40 +1,45 @@
+var size = 4;
+var pos = 0;
+
 function renderer() {
-    useColor(1, 2, COLOR_LIGHT);
-    useColor(1, 3, COLOR_DARK, 6);
-
-    useColor(2, 2, COLOR_LIGHT);
-    useColor(2, 3, COLOR_DARK, 5);
-
-    useColor(3, 2, COLOR_LIGHT);
-    useColor(3, 3, COLOR_DARK, 7);
-
-    useColor(4, 2, COLOR_LIGHT);
-    useColor(4, 3, COLOR_DARK, 7);
-
     CONFIG.sort(function (a, b) {
         return a[1] - b[1];
     });
 
+    useColor(1, 2, COLOR_LIGHT);
+    useColor(2, 2, COLOR_LIGHT);
+    useColor(3, 2, COLOR_LIGHT);
+    useColor(4, 2, COLOR_LIGHT);
+
+    useColor(1, 3, COLOR_DARK, 6);
+    useColor(2, 3, COLOR_DARK, 5);
+    useColor(3, 3, COLOR_DARK, 7);
+    useColor(4, 3, COLOR_DARK, 7);
+
+    useColor(1, 0, pos === 0 ? COLOR_HILIGHT : COLOR_HIDE);
+    useColor(2, 0, pos === 1 ? COLOR_HILIGHT : COLOR_HIDE);
+    useColor(3, 0, pos === 2 ? COLOR_HILIGHT : COLOR_HIDE);
+    useColor(4, 0, pos === 3 ? COLOR_HILIGHT : COLOR_HIDE);
+
     render(
-        'Envelop:\n  Attack:  ' +
+        'Envelop:\n* Attack:  ' +
             Math.pow(CONFIG[0][2], 2) +
-            ' ms\n  Decay:   ' +
+            ' ms\n* Decay:   ' +
             CONFIG[1][2] * 10 +
-            ' ms\n  Sustain: ' +
+            ' ms\n* Sustain: ' +
             Math.round((CONFIG[2][2] / 127) * 100) +
-            '%\n  Release: ' +
+            '%\n* Release: ' +
             Math.pow(CONFIG[3][2], 2) +
             ' ms'
     );
 }
 
 function update(keys) {
-    // if (keys.Up) {
-    //   count++;
-    // } else if (keys.Down) {
-    //   count--;
-    // }
-    // log(JSON.stringify(CONFIG));
+    if (keys.Down) {
+        pos = (pos + 1) % size;
+    } else if (keys.Up) {
+        pos = (pos - 1 + size) % size;
+    }
 
     return VIEW_CHANGED;
 }
